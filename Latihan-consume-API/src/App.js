259c8ecfdaa619c30515cfead2cfb1edfd0ae6da
Hajid Raihan <hajidraihan@gmail.com>
2023-06-24 22:18:44 +0800

    first commit

---
+++ Latihan-consume-API/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import { useEffect, useState } from "react";
+import Product from "./Product";
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div>
+      <Product />
+    </div>
+  );
+}
+
+export default App;
